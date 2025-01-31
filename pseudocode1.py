c777a45df6a1a56327799a60ffbb25336f93b62e
Teddy Putus <teddy.putus@sky.uk>
2025-01-31 15:45:20 +0000

    add scaffolding to pseudocode1
    created outline for create, login and forgot actions

--- pseudocode1.py
+++ pseudocode1.py
@@ -12,7 +12,7 @@ import json
 class Main:
     '''
     payload param is expected to be json {
-        "action": "create/login/forget/update/delete", - mandatory
+        "action": "create/login/forgot/update/delete", - mandatory
         "email": "xx@yy.z", - mandatory 
         "password": "1234", - mandatory for create, login, update, delete
         "newPassword": "5678", - mandatory for update
@@ -31,36 +31,74 @@ class Main:
         if self.act == 'create':
             # code below will be multiple If statements
             # first if: some code that perform email, password validation
-            # second if: do a request to DB to check if a user with same email already exists
-            # if the above checks are successful
-            #   do a request to DB to create a new account and set a pwd in clear
-            #   code to generate a token probably with an external pkg
-            #   return a token and a success message
-            # else
-            #   return an error message
-            pass
+            if self.validate_email() and self.validate_password():
+                # validation passed
+                pass
+                # second if: do a request to DB to check if a user with same email already exists
+                user = self.get_user()
+                if user:
+                    # user exists - error
+                    pass
+                else:
+                    # user doesn't exist
+                    #   do a request to DB to create a new account and set a pwd in clear
+                    #   code to generate a token probably with an external pkg
+                    #   return a token and a success message
+                    if self.create_user():
+                        # user created successfully
+                        return self.generate_token()
+                    else:
+                        # some issue writing to DB
+                        #   return an error message
+                        pass
+            else:
+                # Raise some error
+                pass
+
 
         if self.act == 'login':
             # code below will be multiple If statements
-            # first if: some code that perform email, password
-            # second if: do a request to DB to check if email and pwd are correct
-            # if the above checks are successful
-            #   code to generate a token probably with an external pkg
-            #   return a token and a success message
-            # else
-            #   return an error message
+            # first if: some code that perform email, password validation
+            if self.validate_email() and self.validate_password():
+                # validation passed
+                # second if: do a request to DB to check if email and pwd are correct
+                user = self.get_user()
+                if user:
+                    if self.compare_email(user['email']) and self.compare_password(user['password']):
+                        # second if: do a request to DB to check if email and pwd are correct
+                        return self.generate_token()
+                    else:
+                        # password doesn't match, return error
+                        pass
+                else:
+                    # No such user, return error
+                    pass
+            else:
+                # return an error message
+                pass
             pass
 
-        if self.act == 'forget':
+        if self.act == 'forgot':
             # code below will be multiple If statements
             # first if: some code that perform email validation
-            # second if: do a request to DB to check if email has an account and retrieve password
-            # if the above checks are successful
-            #   code to send an email with password from db probably with an external pkg
-            #   return a success message
-            # else
-            #   return an error message
-            pass
+            if self.validate_email():
+                # second if: do a request to DB to check if email has an account and retrieve password
+                user = self.get_user()
+                if user:
+                    if self.compare_email(user['email']):
+                        #   code to send an email with password from db probably with an external pkg
+                        #   return a success message
+                        self.email_password(user['email'], user['password'])
+                        return True
+                    else:
+                        # No user with that email, return Error
+                        pass
+                else:
+                    # No user with that email, return Error
+                    pass
+            else:
+                # Invalid email, return error
+                pass
 
         if self.act == 'update':
             # code below will be multiple If statements
